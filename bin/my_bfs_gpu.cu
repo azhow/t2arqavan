cb4a1c24aa7a5a165be548de059b82f0ed5f28cc
fgbock <fernandobock1996@gmail.com>
2019-12-02 00:50:56 -0300

    murder

--- bin/my_bfs_gpu.cu
+++ bin/my_bfs_gpu.cu
@@ -11,21 +11,21 @@ using namespace std;
 #define INF 99999
 #define GOAL 5000
 
-__global__ void vecBfs(int nNodes, int** graph, bool *visited, bool *done)
+__global__ void vecBfs(int nNodes, int** graph, bool *visited, bool *notdone)
 {
-	for(int v = blockIdx.x; v < nNodes; ++v){
-            //cout << "visited " << v << endl;
+	for(int v = blockIdx.x * blockDim.x + threadIdx.x; v < nNodes; v+= blockDim.x * gridDim.x){
+            //printf("visited %d\n",v);
             if(v == GOAL)
             {
                 //cout << "Found " << GOAL << endl;
-                *done = true;
+                *notdone = true;
             }
-            for(int i = blockIdx.y; i < nNodes; ++i){
+            for(int i = blockIdx.y * blockDim.y + threadIdx.y; i < nNodes; i += blockDim.y * gridDim.y){
                 if(graph[v][i] != INF && v != i){
                     if(visited[i] == false)
                     {
                         visited[i] = true;
-                        *done = false;
+                        *notdone = false;
                     }
                 }
             }
@@ -62,33 +62,32 @@ int main(int argc, char **argv){
    	queue<int> q;
 	q.push(0);
 	visited[0] = true;
-	
 	/* GPU Setup */
-	int indx = 0, blockSize = 32;
+	int blockSize = 32;
 	int numBlocks = (nNodes + blockSize - 1) / blockSize;
     bool* device_visited;
-    bool* device_done;
-    bool* done;
-	*done = true;
+    bool* device_notdone;
+    bool notdone;
+    notdone = true;
     cudaMalloc(&device_visited, nNodes * sizeof(bool));
     cudaMemcpy(device_visited, visited, nNodes * sizeof(bool),cudaMemcpyHostToDevice);
-    cudaMalloc(&device_done, sizeof(bool));
-    cudaMemcpy(device_done, done, nNodes * sizeof(bool),cudaMemcpyHostToDevice);
-
+    cudaMalloc(&device_notdone, sizeof(bool));
+    cudaMemcpy(device_notdone, &notdone, nNodes * sizeof(bool),cudaMemcpyHostToDevice);
     /* BFS */
     auto start = std::chrono::system_clock::now();
-	while(!(*done)){
-        *done = false;
+	while(notdone){
+        notdone = false;
         /* For all V in Graph - total of nNodes V's*/
-        vecBfs<<<numBlocks,blockSize>>>(nNodes, graph, device_visited, device_done);
+        vecBfs<<<numBlocks,blockSize>>>(nNodes, graph, device_visited, device_notdone);
         cudaDeviceSynchronize();
-        *done = *device_done;
+        cudaMemcpy(&notdone, device_notdone, sizeof(bool),cudaMemcpyDeviceToHost);
     }
     /* BFS DONE */
 
 	auto end = std::chrono::system_clock::now();
 	auto timeElapsed = (end - start);
-	auto sec = std::chrono::duration_cast<std::chrono::seconds>(timeElapsed).count();
+	auto sec = std::chrono::duration_cast<std::chrono::milliseconds>(timeElapsed).count();
+	cout << sec << "\n";
 	for(int i = 0; i < nNodes; i++)
 		free(graph[i]);
 	free(visited);
